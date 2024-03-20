4ef7652c4bb819d82ba888c35c622e941fe8ee85
kazim110 <kazim.mohammadi110@gmail.com>
2024-03-21 00:03:07 +0430

    add association test for state

---
+++ spec/models/state_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe State, type: :model do
+  describe "associations" do
+    it "should belong to a country" do
+      association = described_class.reflect_on_association(:country)
+      expect(association.macro).to eq :belongs_to
+    end
+
+    it "should have many cities" do
+      association = described_class.reflect_on_association(:cities)
+      expect(association.macro).to eq :has_many
+    end
+  end
+end
