d57b967fb718181ddbd96fdd7185262092038f80
kazim110 <kazim.mohammadi110@gmail.com>
2024-03-21 00:00:42 +0430

    Add validation test for city

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe City, type: :model do
       expect(association.macro).to eq :belongs_to
     end
   end
+
+  describe "validations" do
+    it "should validate presence of name" do
+      city = City.new
+      expect(city).not_to be_valid
+      expect(city.errors[:name]).to include("can't be blank")
+    end
+  end
 end
