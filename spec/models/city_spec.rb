bbfe64a263b50760d60beecfe7533c8495f4c964
Juan Carlos Muñoz Navarrete <jcmunav63@gmail.com>
2024-03-20 17:58:37 -0600

    Update city_spec.rb

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  describe "associations" do
-    it "should belong to a state" do
+  describe 'associations' do
+    it 'should belong to a state' do
       association = described_class.reflect_on_association(:state)
       expect(association.macro).to eq :belongs_to
     end
   end
 
-  describe "validations" do
-    it "should validate presence of name" do
+  describe 'validations' do
+    it 'should validate presence of name' do
       city = City.new
       expect(city).not_to be_valid
       expect(city.errors[:name]).to include("can't be blank")
