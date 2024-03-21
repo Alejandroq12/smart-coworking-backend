671342ba95776e7a5eb8bebb0ac2b1cf16300065
Juan Carlos Muñoz Navarrete <jcmunav63@gmail.com>
2024-03-20 18:00:07 -0600

    Update country_spec.rb

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  describe "associations" do
-    it "should have many states" do
+  describe 'associations' do
+    it 'should have many states' do
       association = described_class.reflect_on_association(:states)
       expect(association.macro).to eq :has_many
     end
   end
 
-  describe "validations" do
-    it "should validate presence of name" do
+  describe 'validations' do
+    it 'should validate presence of name' do
       country = Country.new
       expect(country).not_to be_valid
       expect(country.errors[:name]).to include("can't be blank")
