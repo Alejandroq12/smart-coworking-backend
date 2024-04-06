4e52455dc046344cfe5c3cba439ec7bb7b7955cb
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-04-05 21:27:56 -0600

    Feat: Authenticate users before any actions is perform in the users controller.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authenticate_user!
+
       def index
         @users = User.all
         render json: @users, status: :ok, each_serializer: UserSerializer
