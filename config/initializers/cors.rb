7f37a7ee0bef80a70a41f873bd4736ee647ec199
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-12-06 01:07:22 -0600

    Fix: bug.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'https://alejandroq12.github.io' : 'localhost:3000'
+    origins 'https://alejandroq12.github.io', 'localhost:3000', '127.0.0.1:3000'
+
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
